cdf46eb97eb2f9ce085a11854e2ef8c1f32866d3
tanguy leveque <leveque.tanguy@outlook.fr>
2022-04-21 15:26:57 +0200

    connectAPI

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+
+import React, {useState} from 'react'
+import axios from 'axios'
 
 import './App.css';
 import './Navbar.css';
@@ -6,7 +8,7 @@ import './Footer.css';
 
 import Navbar from './componants/Navbar';
 import Footer from './componants/Footer';
-import LoginForm from './componants/formulaire';
+
 
 
 
@@ -15,11 +17,63 @@ import LoginForm from './componants/formulaire';
 
 
 function App() {
+  const [data, setData] = useState({})
+  const [ location, setLocation] = useState ('')
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}6&appid=ea15a20afb45d9e76ea180cd56089bd0`
+
+
+const searchLocation = (e) => {
+  if (e.key === 'Enter') {
+    axios.get(url).then((response) => {
+      setData(response.data)
+      console.log(response.data)
+    })
+    setLocation('')
+  }
+}
+
+
+
   return (
     <>
     <div>
-    <Navbar />
-    <Footer />
+     <Navbar />
+     <div className="container">
+     <div className="search">
+     <input
+     value={location}
+     onChange= {e => setLocation(e.target.value)}
+     onKeyPress={searchLocation}
+     placeholder= 'entrez votre ville'
+     type="text"/>
+     </div>
+
+      <div className="location">
+        <p> La Barben </p>
+      </div>
+      <div className="temp">
+        <h1>30°</h1>
+      </div>
+       <div className="description">
+        <p> nuageux </p>
+      </div>
+     <div className="bottom">
+      <div className="feels">
+        <p className="bold">50°</p>
+         <p> Ressenti </p>
+      </div>
+        <div className="humidity">
+        <p className="bold">20% </p>
+        <p> Humidité</p>
+        </div>
+      <div className="wind">
+          <p className="bold">18 KM/h</p>
+          <p> Vent </p>
+      </div>
+     </div>
+    </div>
+     <Footer />
     </div>
 
     </>
